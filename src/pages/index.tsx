e1b5fd77d96596d340abce9ecb42261d6750bc1f
Kelsi Proulx <kelsiproulx1919@gmail.com>
2023-03-23 10:59:11 -0700

    Fetch data and render images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ allCocktailsData }) {
+  console.log(allCocktailsData.drinks.map((drink) => drink.strDrinkThumb));
   return (
     <>
       <Head>
@@ -17,10 +18,35 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="regular-width-container">
-          <h1>HELLO</h1>
+        <div className="regular-width-container h-screen">
+          {allCocktailsData.drinks.map((drink) => {
+            return (
+              <div key={drink.idDrink}>
+                <Image
+                  src={drink.strDrinkThumb}
+                  alt="Drink thumbnail"
+                  width="100"
+                  height="100"
+                />
+              </div>
+            );
+          })}
         </div>
       </main>
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const resAllCocktails = await fetch(
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+  );
+
+  const allCocktailsData = await resAllCocktails.json();
+
+  return {
+    props: {
+      allCocktailsData,
+    },
+  };
+};
